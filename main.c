78b92dd6dc28a1d1297a4d355f05456beffdcb53
Hoang Minh Khoi <hoangminhkhoi@J293AP.local>
2023-06-25 14:33:55 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,25 +4,34 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <spawn.h>
 
 int main(int argc, char **args) {
   switch (argc) {
-  case 1: {
-    pandash();
-    break;
-  }
-  case 2: {
-    if (fork() == 0) {
-      process_file(args[1]);
-      abort();
-    } else {
-      wait(NULL);
+    case 1: {
+      pandash();
       break;
     }
-  }
-  default: {
-    fprintf(stderr, "TOO MANY ARGS\n");
-    exit(EXIT_FAILURE);
-  }
+    case 2: {
+      posix_spawn_file_actions_t action;
+      posix_spawn_file_actions_init(&action);
+
+      pid_t task_pid;
+      char *command = args[1];
+      char *command_args[] = {command, NULL};
+
+      int spawn_error = posix_spawn(&task_pid, command, &action, NULL, command_args, NULL);
+      if (spawn_error != 0) {
+        fprintf(stderr, "Error executing command: %s\n", command);
+        exit(EXIT_FAILURE);
+      }
+
+      waitpid(task_pid, NULL, 0);
+      break;
+    }
+    default: {
+      fprintf(stderr, "TOO MANY ARGS\n");
+      exit(EXIT_FAILURE);
+    }
   }
 }
